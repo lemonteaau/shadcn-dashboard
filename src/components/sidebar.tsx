a67e97304eadd4ef95a5ff74d095dafe01b67141
Terry Cheng <cyr062599@gmail.com>
2024-05-17 21:15:43 +0930

    chore: Update sidebar layout and add Logo component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,17 +1,21 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
+import Logo from "@/components/logo";
+
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Sidebar({ className }: SidebarProps) {
   return (
-    <div
-      className={cn("flex flex-col justify-center gap-6 px-8 py-8", className)}
-    >
+    <div className={cn("flex flex-col justify-between gap-6 p-12", className)}>
+      <Logo />
       <Button>Home</Button>
       <Button>Supplier</Button>
       <Button>Customer</Button>
       <Button>Product</Button>
+      <div className="h-auto w-full grow"></div>
+      <Button>Settings</Button>
+      <Button>Logout</Button>
     </div>
   );
 }
