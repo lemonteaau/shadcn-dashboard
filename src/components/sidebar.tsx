c97ab8d587fb1db877cec649a46f35146b1b198a
Terry Cheng <cyr062599@gmail.com>
2024-05-18 16:37:30 +0930

    refactor: Update sidebar layout and add icons

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,12 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import {
+  FaHome,
+  FaUserFriends,
+  FaBoxOpen,
+  FaClipboardList,
+} from "react-icons/fa";
+import { MdOutlineSettings, MdOutlineHelpOutline } from "react-icons/md";
 
 import Logo from "@/components/logo";
 
@@ -7,15 +14,46 @@ interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Sidebar({ className }: SidebarProps) {
   return (
-    <div className={cn("flex flex-col justify-between gap-6 p-12", className)}>
+    <div className={cn("flex flex-col justify-between gap-6 p-6", className)}>
       <Logo />
-      <Button>Home</Button>
-      <Button>Supplier</Button>
-      <Button>Customer</Button>
-      <Button>Product</Button>
-      <div className="h-auto w-full grow"></div>
-      <Button>Settings</Button>
-      <Button>Logout</Button>
+      <nav className="flex flex-col gap-2">
+        <Button
+          variant="default"
+          className="flex items-center justify-start gap-2"
+        >
+          <FaHome />
+          <span>Home</span>
+        </Button>
+        <Button variant="ghost" className="flex justify-start gap-2">
+          <FaUserFriends />
+          <span>Supplier</span>
+        </Button>
+        <Button variant="ghost" className="flex justify-start gap-2">
+          <FaClipboardList />
+          <span>Order</span>
+        </Button>
+        <Button variant="ghost" className="flex justify-start gap-2">
+          <FaBoxOpen />
+          <span>Product</span>
+        </Button>
+      </nav>
+      <div className="flex-grow"></div>
+      <nav>
+        <Button
+          variant="ghost"
+          className="flex items-center justify-start gap-2"
+        >
+          <MdOutlineHelpOutline />
+          <span>Help</span>
+        </Button>
+        <Button
+          variant="ghost"
+          className="flex items-center justify-start gap-2"
+        >
+          <MdOutlineSettings />
+          <span>Settings</span>
+        </Button>
+      </nav>
     </div>
   );
 }
