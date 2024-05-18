54e763861250f653aca07393262875a99db09e4f
Terry Cheng <cyr062599@gmail.com>
2024-05-18 20:07:34 +0930

    refactor: Update dashboard layout and components

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,15 +1,174 @@
+"use client";
+
+import * as React from "react";
+import { addDays, format } from "date-fns";
+import {
+  Calendar as CalendarIcon,
+  DollarSign,
+  CreditCard,
+  Users,
+} from "lucide-react";
+import { DateRange } from "react-day-picker";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { cn } from "@/lib/utils";
+import { Overview } from "./overview";
+
+function DatePickerWithRange({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  });
+
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-[300px] justify-start text-left font-normal",
+              !date && "text-muted-foreground",
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
+
+function CardWrapperDemo() {
+  return (
+    <div className="grid grid-cols-3 gap-4 p-6">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+          <DollarSign className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">$45,231.89</div>
+          <p className="text-xs text-muted-foreground">
+            +20.1% from last month
+          </p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
+          <Users className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">+2350</div>
+          <p className="text-xs text-muted-foreground">
+            +180.1% from last month
+          </p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Sales</CardTitle>
+          <CreditCard className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">+12,234</div>
+          <p className="text-xs text-muted-foreground">+19% from last month</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+function SelectDemo() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select..." />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Seletion</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
 
 interface DashboardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function Dashboard({ className }: DashboardProps) {
   return (
     <div className={cn("m-7 flex-grow space-y-8", className)}>
-      <div className="rounded-2xl bg-white">
-        <h1>Dashboard 1</h1>
+      <div className="flex flex-col rounded-2xl bg-white">
+        <div className="flex items-center justify-between px-6 pt-8">
+          <span className="text-xl font-bold">Dashboard</span>
+          <div className="flex gap-2">
+            <DatePickerWithRange />
+            <Button>Download</Button>
+          </div>
+        </div>
+        <CardWrapperDemo />
+        <div className="flex items-center justify-between px-6">
+          <span className="text-xl font-bold">Overview</span>
+          <SelectDemo />
+        </div>
+        <div className="m-6 rounded-2xl border p-4">
+          <Overview />
+        </div>
       </div>
-      <div className="rounded-2xl bg-white">
-        <h1>Dashboard 2</h1>
+      <div className="flex flex-col rounded-2xl bg-white">
+        <div className="flex items-center justify-between px-6 pt-8">
+          <span className="text-xl font-bold">Another Demo</span>
+        </div>
+        <CardWrapperDemo />
       </div>
     </div>
   );
