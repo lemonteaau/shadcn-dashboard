59b2dcaa6eaff5cc2e129cdbffdc78af3bd94ddd
Terry Cheng <cyr062599@gmail.com>
2024-05-18 16:36:12 +0930

    refactor: Update page layout and add Topbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import Logo from "@/components/logo";
 import Sidebar from "@/components/sidebar";
+import { Topbar } from "@/components/topbar";
 
 export default function Home() {
   return (
     <main className="grid h-screen grid-cols-5">
-      <Sidebar className="col-span-1 h-full" />
-      <div className="col-span-4">
-        <h1>Dashboard page</h1>
+      <Sidebar className="col-span-1 border-r" />
+      <div className="col-span-4 bg-gray-100">
+        <Topbar className="h-24 bg-white" />
+        <div className="pt-5">
+          <h1>Dashboard page</h1>
+        </div>
       </div>
     </main>
   );
